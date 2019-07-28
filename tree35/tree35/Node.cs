5352ba7e265a4a742f9f438704de13e9d8dd3669
shadowray1999 <slava.pavlenko2010@yandex.com>
2019-07-28 18:51:09 +0400

    updated tree output

---
+++ tree35/tree35/Node.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree35
+{
+    class Node
+    {
+        public int value { get; set; }
+        public Node leftChild { get; set; }
+        public Node rightChild { get; set; }
+        public Node(int value)
+        {
+            this.value = value;
+        }
+        public void addLeft(int value)
+        {
+            this.leftChild = new Node(value);
+        }
+        public void addRight(int value)
+        {
+            this.rightChild = new Node(value);
+        }
+        public void treeOut(int offset, int level, int deep)  //offset - отступ слева, level - отступ сверху
+        {
+            Console.SetCursorPosition(offset, level);
+            Console.Write(this.value);
+            if (leftChild != null)
+            {
+                int curPos = offset - 2;
+                int childPos = -1;
+                childPos = (int)(curPos - Console.WindowWidth / Math.Pow(2, deep + 2));
+                switch (deep)
+                {
+                    case 0:
+                        childPos += 8;
+                        break;
+                    case 1:
+                        childPos += 5;
+                        break;
+                    case 2:
+                        childPos += 2;
+                        break;
+                    case 3:
+                        childPos += 1;
+                        break;
+                }
+                if (childPos >= 0)
+                {
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("/");
+                    while (curPos > childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos--;
+                    }
+                    this.leftChild.treeOut(childPos - 1, level + 2, deep + 1);
+                }
+            }
+            if (rightChild != null)
+            {
+                int curPos = offset + 2;
+                int childPos = -1;
+                childPos = (int)(curPos + Console.WindowWidth / Math.Pow(2, deep + 2));
+                switch (deep)
+                {
+                    case 0:
+                        childPos -= 8;
+                        break;
+                    case 1:
+                        childPos -= 5;
+                        break;
+                    case 2:
+                        childPos -= 2;
+                        break;
+                    case 3:
+                        childPos -= 1;
+                        break;
+                }
+                if (childPos >= 0)
+                {
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("\\");
+                    while (curPos < childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos++;
+                    }
+                    this.rightChild.treeOut(childPos + 1, level + 2, deep + 1);
+                }
+            }
+            /*
+            Console.SetCursorPosition(offset, level);
+            Console.Write(this.value);
+            if (leftChild != null)
+            {
+                //построение линии
+                int curPos = offset - 2; // от текущего значения
+                if (curPos < Console.WindowWidth / 2)   //для левых детей в левом поддереве
+                {
+                    int childPos = offset / 2 + 1;   //до левого ребенка
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("/");
+
+                    while (curPos > childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos--;
+                    }
+                    this.leftChild.treeOut(offset / 2, level + 2);
+                }
+                else  //для левых детей в правом поддереве
+                {
+                    int childPos = curPos - (Console.WindowWidth / 2 - offset / 2) + 2;   //до левого ребенка
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("/");
+
+                    while (curPos > childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos--;
+                    }
+                    this.leftChild.treeOut(childPos - 1, level + 2);
+                }
+            }
+            if (rightChild != null)
+            {
+                int curPos = offset + 2; // от текуoего значения
+                if (curPos > Console.WindowWidth / 2)   //для правых детей в правом поддереве
+                {
+                    int childPos = offset / 2 + Console.WindowWidth / 2 - 1;   //до правого ребенка
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("\\");
+                    while (curPos < childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos++;
+                    }
+                    this.rightChild.treeOut(offset / 2 + Console.WindowWidth / 2, level + 2);
+                }
+                else  //для правых детей в левом поддереве
+                {
+                    int childPos = curPos + offset / 2 - 2;   //до правого ребенка
+                    Console.SetCursorPosition(childPos, level + 1);
+                    Console.Write("\\");
+
+                    while (curPos < childPos)
+                    {
+                        Console.SetCursorPosition(curPos, level);
+                        Console.Write("_");
+                        curPos++;
+                    }
+                    this.rightChild.treeOut(childPos + 1, level + 2);
+                }
+            }
+            */
+        }
+    }
+}
---
+++ tree35/tree35/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree35
+{
+    class Program
+    {
+        static int k = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начальное дерево: ");
+            //заполнение массива значениями
+            Random random = new Random();
+            int[] mas = new int[8];
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = random.Next(10);
+            //максимальное кол-во вершин
+            double max = 0;
+            for (double j = 0; j < 3; j++)
+                max += Math.Pow(2, j);
+            //формирование дерева
+            Node root = new Node(mas[0]);
+            root.leftChild = new Node(mas[1]);
+            root.rightChild = new Node(mas[2]);
+            root.rightChild.rightChild = new Node(mas[3]);
+            root.rightChild.leftChild = new Node(mas[4]);
+            root.leftChild.leftChild = new Node(mas[5]);
+            root.leftChild.leftChild.leftChild = new Node(mas[6]);
+            root.leftChild.leftChild.rightChild = new Node(mas[7]);
+
+            root.treeOut(Console.WindowWidth / 2, 2, 0);
+            Console.SetCursorPosition(0, 10);
+            Console.WriteLine("Конечное дерево: ");
+            double_nodes_value(root);
+            root.treeOut(Console.WindowWidth / 2, 12, 0);
+            Console.Read();
+        }
+        static void double_nodes_value(Node node)
+        {
+            node.value = node.value * 2;
+            if (node.leftChild != null)
+                double_nodes_value(node.leftChild);
+            if (node.rightChild != null)
+                double_nodes_value(node.rightChild);
+        }
+        static void formin_tree(int nodes_amount, int tree_level, double max, int tmp_lvl_1, Node node, int[] value_mas)
+        {
+            if (tmp_lvl_1 < tree_level && k < nodes_amount && k <= max)
+            {
+                if (node.leftChild == null)
+                {
+                    tmp_lvl_1++;
+                    node.leftChild = new Node(value_mas[k]);
+                    k++;
+                    formin_tree(nodes_amount, tree_level, max, tmp_lvl_1, node.leftChild, value_mas);
+                    tmp_lvl_1--;
+                }
+                if (node.rightChild == null)
+                {
+                    tmp_lvl_1++;
+                    node.rightChild = new Node(value_mas[k]);
+                    k++;
+                    formin_tree(nodes_amount, tree_level, max, tmp_lvl_1, node.rightChild, value_mas);
+                    tmp_lvl_1--;
+                }
+            }
+        }
+    }
+}
